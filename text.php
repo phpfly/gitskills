c494f28bbed410ed33b2800b629c54f201b9d1b8
zyk_done <zyk_done@163.com>
2017-06-30 10:19:19 +0800

    edit text.php

--- text.php
+++ text.php
@@ -2,4 +2,24 @@
 
 $a = '123';
 
-echo gettype($a);
+echo gettype($a),'<br>';
+
+echo 'error_code=3801';echo '<br>';
+echo 'error_code&nbsp=&nbsp3801';
+
+echo '<hr>';
+var_dump($_GET);
+
+//$string = str_replace('=', ' = ', $_GET['test']);
+var_dump($string);
+
+$str = <<<'EOD'
+
+EOD;
+
+$str1 = <<<EOD
+
+EOD;
+
+
+echo base64_encode('Kang');
